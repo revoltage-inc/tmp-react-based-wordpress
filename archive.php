3655e26c41548d345eb63c56789db421a576a36f
yogarasu <ishiguro@revoltage.jp>
2023-04-12 11:42:01 +0900

    fix #2 ファイルが存在しないときの処理を追加

--- archive.php
+++ archive.php
@@ -1,5 +1,5 @@
 <?php
 $post_type = get_query_var('post_type');
-if (is_date($post_type) || is_archive($post_type)) {
+if ((is_date($post_type) || is_archive($post_type)) && file_exists("archives/{$post_type}.php")) {
   include_once("archives/{$post_type}.php");
 }
